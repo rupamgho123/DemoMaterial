8915e4981edacc25be46705f803bcf910d042dcc
RupamGhosh <rupam.ghosh@flipkart.com>
2016-05-10 17:20:41 +0530

    more code

--- app/src/main/java/com/example/demomaterial/MainActivity.java
+++ app/src/main/java/com/example/demomaterial/MainActivity.java
@@ -1,17 +1,6 @@
 package com.example.demomaterial;
 
-import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
 
@@ -22,13 +11,11 @@ public class MainActivity extends BaseActivity {
         openFragment1();
     }
 
-    private void openFragment1(){
+    public void openFragment1(){
         getSupportFragmentManager().beginTransaction().replace(R.id.contentFragment,Fragment1.getInstance()).commit();
     }
 
-//    private void openFragment2(){
-//        getSupportFragmentManager().beginTransaction().replace(,Fragment1.getInstance()).commit();
-//    }
-
-
+    public void openFragment2(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.contentFragment,Fragment2.getInstance()).commit();
+    }
 }
