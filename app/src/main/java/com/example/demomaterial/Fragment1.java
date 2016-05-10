ff8565ff1199287831dd289620d9982d8d075382
RupamGhosh <rupam.ghosh@flipkart.com>
2016-05-10 16:25:33 +0530

    added code

---
+++ app/src/main/java/com/example/demomaterial/Fragment1.java
@@ -0,0 +1,71 @@
+package com.example.demomaterial;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by rupam.ghosh on 10/05/16.
+ */
+public class Fragment1 extends Fragment {
+
+    public static Fragment1 getInstance(){
+        return new Fragment1();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_main,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Set a toolbar to replace the action bar.
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        AppCompatActivity act = (AppCompatActivity)getActivity();
+        act.setSupportActionBar(toolbar);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(new MainAdapter());
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+    }
+
+    private class MainListHolder extends RecyclerView.ViewHolder{
+
+        TextView textView;
+
+        public MainListHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.textView);
+        }
+    }
+
+    private class MainAdapter extends RecyclerView.Adapter<MainListHolder>{
+
+        @Override
+        public MainListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MainListHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_layout,parent,false));
+        }
+
+        @Override
+        public void onBindViewHolder(MainListHolder holder, int position) {
+            holder.textView.setText(""+position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 15;
+        }
+    }
+}
--- app/src/main/java/com/example/demomaterial/MainActivity.java
+++ app/src/main/java/com/example/demomaterial/MainActivity.java
@@ -15,94 +15,16 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Set a toolbar to replace the action bar.
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setAdapter(new MainAdapter());
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(llm);
-
-
-//        int screenSize = getResources().getConfiguration().screenLayout &
-//                Configuration.SCREENLAYOUT_SIZE_MASK;
-//
-//        switch(screenSize) {
-//            case Configuration.SCREENLAYOUT_SIZE_XLARGE:
-//                Toast.makeText(this, "XLarge screen", Toast.LENGTH_LONG).show();
-//                break;
-//            case Configuration.SCREENLAYOUT_SIZE_LARGE:
-//                Toast.makeText(this, "Large screen", Toast.LENGTH_LONG).show();
-//                break;
-//            case Configuration.SCREENLAYOUT_SIZE_NORMAL:
-//                Toast.makeText(this, "Normal screen",Toast.LENGTH_LONG).show();
-//                break;
-//            case Configuration.SCREENLAYOUT_SIZE_SMALL:
-//                Toast.makeText(this, "Small screen",Toast.LENGTH_LONG).show();
-//                break;
-//            default:
-//                Toast.makeText(this, "Screen size is neither large, normal or small" , Toast.LENGTH_LONG).show();
-//        }
-
-        int density= getResources().getDisplayMetrics().densityDpi;
-
-        switch(density)
-        {
-            case DisplayMetrics.DENSITY_LOW:
-                Toast.makeText(this, "LDPI", Toast.LENGTH_SHORT).show();
-                break;
-            case DisplayMetrics.DENSITY_MEDIUM:
-                Toast.makeText(this, "MDPI", Toast.LENGTH_SHORT).show();
-                break;
-            case DisplayMetrics.DENSITY_HIGH:
-                Toast.makeText(this, "HDPI", Toast.LENGTH_SHORT).show();
-                break;
-            case DisplayMetrics.DENSITY_XHIGH:
-                Toast.makeText(this, "XHDPI", Toast.LENGTH_SHORT).show();
-                break;
-            case DisplayMetrics.DENSITY_XXHIGH:
-                Toast.makeText(this, "XXHDPI", Toast.LENGTH_SHORT).show();
-                break;
-            case DisplayMetrics.DENSITY_XXXHIGH:
-                Toast.makeText(this, "XXXHDPI", Toast.LENGTH_SHORT).show();
-                break;
-        }
     }
 
-    private class MainListHolder extends RecyclerView.ViewHolder{
-
-        TextView textView;
-
-        public MainListHolder(View itemView) {
-            super(itemView);
-            textView = (TextView) itemView.findViewById(R.id.textView);
-        }
+    private void openFragment1(){
+        getSupportFragmentManager().beginTransaction().replace(,Fragment1.getInstance()).commit();
     }
 
-    private class MainAdapter extends RecyclerView.Adapter<MainListHolder>{
-
-        @Override
-        public MainListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new MainListHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_layout,parent,false));
-        }
-
-        @Override
-        public void onBindViewHolder(MainListHolder holder, int position) {
-            holder.textView.setText(""+position);
-        }
-
-        @Override
-        public int getItemCount() {
-            return 15;
-        }
-    }
+//    private void openFragment2(){
+//        getSupportFragmentManager().beginTransaction().replace(,Fragment1.getInstance()).commit();
+//    }
 }
